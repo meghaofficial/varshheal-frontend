ea099a8beb93be80317b2e1724c52b5077394f64
meghasharma <meghaofficial789@gmail.com>
2025-10-02 13:02:25 +0530

    added backend url

--- src/utils/axiosPublic.js
+++ src/utils/axiosPublic.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosPublic = axios.create({
-  baseURL: 'http://localhost:8000/api', 
+  // baseURL: 'http://localhost:8000/api', 
+  baseURL: 'https://varshheal-backend.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
